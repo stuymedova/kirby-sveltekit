6350097c17b0ce588850f019b40c406cc6e464b8
Sofya Tuymedova <stuymedova@gmail.com>
2021-05-13 18:35:14 +0300

    updated env

--- src/routes/api/query.js
+++ src/routes/api/query.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { KIRBY_URL, KIRBY_USER_EMAIL, KIRBY_USER_PASSWORD } = process.env;
+const { API_URL, API_USER_EMAIL, API_USER_PASSWORD } = process.env;
 
 export async function post(req) {
-  const url = KIRBY_URL;
-  const auth = Buffer.from(`${KIRBY_USER_EMAIL}:${KIRBY_USER_PASSWORD}`).toString('base64');
+  const url = API_URL;
+  const auth = Buffer.from(`${API_USER_EMAIL}:${API_USER_PASSWORD}`).toString('base64');
 
   const res = await fetch(url, {
     method: 'POST',
